5e81611b0350bee42d0af75064677c4ddd11cf9a
Glitch (tulip-rain-gong) <none>
2020-07-21 07:53:23 +0000

    💫🏍 Checkpoint ./commands/owner/restart.js:14049126/642

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,11 +1,11 @@
 module.exports = {
-    name: "restart",
-    category: "owner",
-    run: async (client, message, args) => {
-        if (message.author.id !== '598935312491741197') {
-            return message.channel.send(`You cannot use this command!`)
-        }
-        await message.channel.send(`Restarting bot...`)
-        process.exit();
+  name: "restart",
+  category: "owner",
+  run: async (client, message, args) => {
+    if (message.author.id !== "185957154606284800") {
+      return message.channel.send(`You cannot use this command!`);
     }
-}
+    await message.channel.send(`Restarting bot...`);
+    process.exit();
+  }
+};
