1a7aa053eefbddfd55c8f9e10c1c6271139dddf2
Glitch (tulip-rain-gong) <none>
2020-07-21 07:17:30 +0000

    🐝🚧 Checkpoint ./commands/fun/math.js:14049126/818 ./commands/image/neko.js:14049126/989

---
+++ commands/fun/math.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+const math = require("mathjs");
+const Color = `RANDOM`;
+
+module.exports = {
+  name: "math",
+  category: "fun",
+  run: async (client, message, args) => {
+    try {
+      if (!args[0]) return message.channel.send("Please Give Me Equation!");
+
+      const embed = new MessageEmbed()
+        .setColor(`${Color}`)
+        .setFooter(
+          `Tamako`,
+          bot.user.
+        )
+        .setTitle(`**Jawabannya Adalah**`)
+        .setDescription(math.evaluate(args.join(" ")))
+        .setTimestamp();
+
+      message.channel.send(embed);
+    } catch (error) {
+      message.channel.send(`Please Give Me Valid Equation | Try Again Later!`).then(() => console.log(error));
+    }
+  }
+};
---
+++ commands/image/neko.js
@@ -0,0 +1,33 @@
+//_______________________________________________________________
+const Discord = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "neko",
+  description: "Gratis Neko Bos",
+  category: "image",
+  aliases: ["kucing"],
+  run: async (bot, message, args) => {
+
+    fetch("https://nekos.life/api/v2/img/neko")
+      .then(res => res.json())
+      .then(body => {
+        if (!body)
+          return message.channel.send("**Aduh Kok Gk Bisa Ya? Coba Ulangin**");
+
+        let embed = new Discord.MessageEmbed()
+          .setAuthor(`Yey Ada Neko`)
+          .setTitle(
+            `*Udah Donlod Aja Udah*`
+          )
+          .setImage(body.url)
+          .setFooter(
+            `Tamako`,
+            bot.user.displayAvatarURL()
+          );
+
+        return message.channel.send(embed);
+      });
+  }
+};
+//_______________________________________________________________
