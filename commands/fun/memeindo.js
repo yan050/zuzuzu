e2287d0c32411fa7d01fd87a909fb20f3e9a7d9d
Glitch (tulip-rain-gong) <none>
2020-07-21 10:31:59 +0000

    🍋😹 Checkpoint ./commands/fun/memeindo.js:14049126/43

--- commands/fun/memeindo.js
+++ commands/fun/memeindo.js
@@ -1,14 +1,14 @@
 const { MessageEmbed } = require("discord.js");
 const api = require("imageapi.js");
 module.exports = {
-  name: "memeindo",
+  name: "indo",
   description: "Get a panda",
   category: "fun",
   run: async (bot, message, args) => {
     let subreddits = ["indonesia"];
     let img = await api(subreddits);
     const Embed = new MessageEmbed()
-      .setTitle(`Meme Indo Boiss`)
+      .setTitle(`Indonesiaa!!`)
       .setURL(`https://reddit.com/r/${subreddits}`)
       .setColor("RANDOM")
       .setImage(img);
