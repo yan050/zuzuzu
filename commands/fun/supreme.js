c02e950ad7c70a7fd5db87c00718d4c88f217c3a
Glitch (tulip-rain-gong) <none>
2020-07-22 01:17:21 +0000

    🍪🌟 Checkpoint ./commands/fun/supreme.js:14049126/603

---
+++ commands/fun/supreme.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+module.exports = {
+  name :"supreme",
+  category: "fun",
+  description: "Membuat Text Menjadi Suprim",
+run: async (client, message, args) => {
+if (!args[0]) return message.channel.send("Please Add Text To Supremeify");
+        
+        const supreme = new Discord.MessageEmbed()
+        .setColor('#de0000')
+        .setTitle('Nih\'s Teks Supreme Nya :v')
+        .setImage(`https://api.alexflipnote.dev/supreme?text=${args.join("%20")}`)
+        .setFooter(`Requested By ${message.author.tag}`)
+        .setTimestamp();
+        
+message.channel.send(supreme);
+}
+}
