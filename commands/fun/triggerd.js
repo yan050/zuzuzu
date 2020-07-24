ea3e2d471cbdfed3bbedb26db26ec1e500e8fca8
Glitch (tulip-rain-gong) <none>
2020-07-24 15:25:41 +0000

    🌓📺 Checkpoint ./commands/fun/triggerd.js:14049126/495

--- commands/fun/triggerd.js
+++ commands/fun/triggerd.js
@@ -5,16 +5,16 @@ const canvas = new Canvacord();
 const { MessageAttachment } = require("discord.js");
 
 module.exports = {
-  name: "magik",
+  name: "triggerd",
 run: async (client, message, args) => {
 
     const user = message.mentions.users.first() || message.author;
 
-    const image = await canvas.magik(user.displayAvatarURL({ format: "png" }));
+    const image = await canvas.trigger(user.displayAvatarURL({ format: "png" }));
 
     const attachment = new MessageAttachment(image, "triggered.gif");
 
     return message.channel.send(attachment);
 
 }
-}
+  }
