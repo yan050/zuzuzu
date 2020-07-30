c08102188178e1a389952ab37346fb5c60a60c5a
Glitch (tulip-rain-gong) <none>
2020-07-30 23:58:35 +0000

    🚶💸 Checkpoint ./commands/fun/triggerd.js:14049126/80

--- commands/fun/triggerd.js
+++ commands/fun/triggerd.js
@@ -10,7 +10,7 @@ run: async (client, message, args) => {
 
     const user = message.mentions.users.first() || message.author;
 
-    const image = await canvas.trigger(user.displayAvatarURL({ format: "png" }));
+    const image = await canvas.youtube(user.displayAvatarURL({ format: "png" }));
 
     const attachment = new MessageAttachment(image, "triggered.gif");
 
