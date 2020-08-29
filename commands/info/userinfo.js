260ab0cf687a2459f5b2b42ffb340b2a7bfb1342
Glitch (tulip-rain-gong) <none>
2020-08-29 18:11:39 +0000

    Rewound to commit 24d251f63c457cf2f1121cc132f234878c133755 + 0 edits

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -1,86 +1,67 @@
-const Discord = require('discord.js');
-const moment = require("moment");
-
-const status = {
-    online: "Online",
-    idle: "Idle",
-    dnd: "Do Not Disturb",
-    offline: "Offline/Invisible"
-};
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.json");
 module.exports = {
   name: "userinfo",
-  description: "Menampilkan Informasi Dari User Tersebut",
-  category: "info",
-  aliases: ["ui"],
-run: (client, message, args) =>{
-    var permissions = [];
-    var acknowledgements = 'None';
-   
-    const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-    const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); }); 
+  aliases: ["memberinfo", "whois"],
+  category: "utility",
+  description: "Show User Information!",
+  usage: "Userinfo | <Mention User>",
+  accessableby: "everyone",
+  run: async (client, message, args) => {
+
+    //Start
+
+    const member = message.mentions.members.first() ||
+
+      message.member;
+
+    const statuses = {
+
+      online: 'Online',
+
+      dnd: 'Do Not Disturb',
+
+      idle: 'Idle',
+
+      offline: 'Offline/Invisible',
+
+    };
+
+    const embed = new MessageEmbed()
+
+      .setTitle(member.user.username + " Information!")
+
+      .setColor(Color)
+
+      .setThumbnail(member.user.displayAvatarURL())
+
+      .addField('Full Name', member.user.tag, true)
+
+      .addField('ID', `${member.id}`, true)
+
+      .addField('Status', statuses[member.presence.status], true)
+
+      .addField(`Highest Role`, `${member.roles.highest || "No Role!"}`, true)
+
+      .addField(`Roles Count`, message.guild.members.cache.get(member.user.id).roles.cache.size || "No Roles!", true)
+
+      .addField(`Avatar Url`, `[Link](${member.user.displayAvatarURL()})`, true)
+
+      .addField('Joined Server At', member.joinedAt.toDateString())
+
+      .addField('Joined Discord At', member.user.createdAt.toDateString())
+
+      .setFooter(`Requested by ${message.author.username}`)
+
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+
     
-    if(message.member.hasPermission("KICK_MEMBERS")){
-        permissions.push("Kick Members");
-    }
-    
-    if(message.member.hasPermission("BAN_MEMBERS")){
-        permissions.push("Ban Members");
-    }
-    
-    if(message.member.hasPermission("ADMINISTRATOR")){
-        permissions.push("Administrator");
-    }
 
-    if(message.member.hasPermission("MANAGE_MESSAGES")){
-        permissions.push("Manage Messages");
-    }
-    
-    if(message.member.hasPermission("MANAGE_CHANNELS")){
-        permissions.push("Manage Channels");
-    }
-    
-    if(message.member.hasPermission("MENTION_EVERYONE")){
-        permissions.push("Mention Everyone");
-    }
+  }
 
-    if(message.member.hasPermission("MANAGE_NICKNAMES")){
-        permissions.push("Manage Nicknames");
-    }
-
-    if(message.member.hasPermission("MANAGE_ROLES")){
-        permissions.push("Manage Roles");
-    }
-
-    if(message.member.hasPermission("MANAGE_WEBHOOKS")){
-        permissions.push("Manage Webhooks");
-    }
-
-    if(message.member.hasPermission("MANAGE_EMOJIS")){
-        permissions.push("Manage Emojis");
-    }
-
-    if(permissions.length == 0){
-        permissions.push("No Key Permissions Found");
-    }
-
-    if(member.user.id == message.guild.ownerID){
-        acknowledgements = 'Server Owner';
-    }
-
-    const embed = new Discord.MessageEmbed()
-        .setDescription(`<@${member.user.id}>`)
-        .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
-        .setColor(randomColor)
-        .setFooter(`ID: ${message.author.id}`)
-        .setThumbnail(member.user.displayAvatarURL)
-        .setTimestamp()
-        .addField(`Status`,`${status[member.user.presence.status]}`, true)
-        .addField(`Masuk Pada Tanggal :`,`${moment(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-        .addField(`Akun Dibuat Pada Tanggal :`,`${moment(message.author.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-        .addField(`Roles [${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).length}]`,`${member.roles.cache.filter(r => r.id !== message.guild.id).map(roles => `<@&${roles.id }>`).join(" **|** ") || "No Roles"}`, true)
-        .addField("Acknowledgements: ", `${acknowledgements}`, true);
-        
-    message.channel.send({embed});
-
-}
-}
+};
